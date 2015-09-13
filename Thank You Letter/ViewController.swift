16b31a8597a30fd8b59dc8ae660b8d22c6c29434
le roncier <leroncier@gmail.com>
2015-09-13 19:47:13 +0200

    Initial Commit

---
+++ Thank You Letter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Thank You Letter
+//
+//  Created by Charles Roncier on 13/09/2015.
+//  Copyright © 2015 Charles Roncier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
